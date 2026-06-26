063dbae62b2172ea6337024bacf672560e322d5d
Quyenntn_2207 <ngotunhaquyen1706@gmail.com>
2026-06-26 14:15:24 +0700

    Update FPGA_GEMM.h

--- FPGA/FPGA_GEMM.h
+++ FPGA/FPGA_GEMM.h
@@ -1,30 +1,34 @@
-#pragma once
+#ifndef FPGA_GEMM_H
+#define FPGA_GEMM_H
+
 #include <stdbool.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef enum {
+typedef enum FpgaQuantType {
     FPGA_QUANT_Q5_0 = 0,
     FPGA_QUANT_Q8_0 = 1,
     FPGA_QUANT_Q4_K = 2,
 } FpgaQuantType;
 
-bool fpga_gemm_init();
-void fpga_gemm_cleanup();
-bool fpga_gemm_is_ready();
+bool fpga_gemm_init(void);
+bool fpga_gemm_is_ready(void);
+void fpga_gemm_shutdown(void);
 
-void fpga_gemm_run(const void *  A_raw,  // weight quantized [K × N]
+void fpga_gemm_run(const void *  A_raw,
                    FpgaQuantType quant,
-                   const float * B_f32,  // activation F32   [M × K]
-                   float *       C,      // output F32        [M × N]
+                   const float * B_f32,
+                   float *       C,
                    int           M,
                    int           K_orig,
                    int           N_orig,
-                   int           shift  // 0 = auto
-);
+                   int           shift_in);
 
 #ifdef __cplusplus
 }
 #endif
+
+#endif  // FPGA_GEMM_H
